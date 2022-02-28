8262b38406a3570805b3605eb8a3b255bf53fbcf
dmark1415 <dmark15@protonmail.com>
2022-02-28 16:04:20 -0600

    Added Incept SDK functions

--- sdk/src/incept.ts
+++ sdk/src/incept.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { BN, Program } from "@project-serum/anchor";
-import { Incept, IDL } from './idl/exchange'
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { Incept, IDL } from "./idl/exchange";
 import {
   PublicKey,
   Connection,
@@ -9,17 +10,20 @@ import {
   ConfirmOptions,
 } from "@solana/web3.js";
 
-export class Exchange {
+const RENT_PUBKEY = anchor.web3.SYSVAR_RENT_PUBKEY;
+const SYSTEM_PROGRAM_ID = anchor.web3.SystemProgram.programId;
+
+export class Incept {
   connection: Connection;
   network: Network;
   wallet: Wallet;
   programId: PublicKey;
   exchangeAuthority: PublicKey;
-  program: Program<ExchangeType>;
+  program: Program<Incept>;
   manager: Manager;
   tokenData: TokenData;
   opts?: ConfirmOptions;
-  stateAddress: PublicKey;
+  managerPubkey: PublicKey;
 
   private constructor(
     connection: Connection,
@@ -29,7 +33,7 @@ export class Exchange {
     programId = PublicKey.default,
     opts?: ConfirmOptions
   ) {
-    this.stateAddress = PublicKey.default;
+    this.managerPubkey = PublicKey.default;
     this.manager = {} as Manager;
     this.tokenData = {} as TokenData;
     this.connection = connection;
@@ -66,6 +70,101 @@ export class Exchange {
         throw new Error("Not supported");
     }
   }
+
+  public async initializeManager(admin) {
+    const [managerPubkey, bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("manager")],
+      this.program.programId
+    );
+    const usdiMint = anchor.web3.Keypair.generate();
+    const liquidatedCometUsdiTokenAccount = anchor.web3.Keypair.generate();
+    const tokenData = anchor.web3.Keypair.generate();
+
+    await this.program.rpc.initializeManager(bump, {
+      accounts: {
+        admin: admin,
+        manager: managerPubkey,
+        usdiMint: usdiMint.publicKey,
+        liquidatedCometUsdiTokenAccount:
+          liquidatedCometUSDITokenAccount.publicKey,
+        exchangeAuthority: exchangeAuthority,
+        tokenData: tokenDataAccount.publicKey,
+        rent: RENT_PUBKEY,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SYSTEM_PROGRAM_ID,
+      },
+    });
+    this.managerPubkey = managerPubkey;
+  }
+
+  public onManagerAccountChange(fn: (state: Manager) => void) {
+    this.program.account.manager
+      .subscribe(this.manager)
+      .on("change", (state: Manager) => {
+        fn(state);
+      });
+  }
+
+  public onTokenDataChange(fn: (state: TokenData) => void) {
+    this.program.account.tokenData
+      .subscribe(this.manager.tokenData)
+      .on("change", (state: TokenData) => {
+        fn(state);
+      });
+  }
+
+  public async initializeUser(userWalletAddress) {}
+
+  public async addCollateral(admin) {}
+
+  public async addPool(admin) {}
+
+  public async updatePrices() {
+    const tokenData = await this.getTokenData(this.manager.tokenData);
+    const priceFeeds = tokenData.pools
+      .filter(
+        (pool) => !pool.assetInfo.priceFeedAddress.equals(DEFAULT_PUBLIC_KEY)
+      )
+      .map((asset) => {
+        return {
+          pubkey: pool.assetInfo.priceFeedAddress,
+          isWritable: false,
+          isSigner: false,
+        };
+      });
+    return await this.program.rpc.updatePrices({
+      remainingAccounts: priceFeeds,
+      accounts: {
+        manager: this.managerPubkey,
+        tokenData: this.manager.tokenData,
+      },
+    });
+  }
+
+  public async getTokenData() {}
+
+  public async getManagerAddress() {
+    const [managerPubkey, bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(utils.bytes.utf8.encode("manager"))],
+      this.program.programId
+    );
+    return { managerPubkey, bump };
+  }
+
+  public async getUserAddress(userWalletAddress) {
+    const [userPubkey, bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("user"), userWalletAddress.toBuffer()],
+      this.program.programId
+    );
+    return { userPubkey, bump };
+  }
+}
+
+export interface Manager {
+  usdiMint: PublicKey;
+  liquidatedCometUsdi: PublicKey;
+  tokenData: PublicKey;
+  admin: PublicKey;
 }
 
 export interface TokenData {
