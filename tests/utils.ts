6efe33ba9c26da48d8dc9c9c9e3c5bb5448ffc38
Andrew Tan <aronyeager@protonmail.com>
2022-03-03 17:23:08 -0500

    WIP updating tests.

---
+++ tests/utils.ts
@@ -0,0 +1,11 @@
+import {
+    Connection,
+    Keypair,
+    PublicKey,
+    SYSVAR_RENT_PUBKEY,
+    Transaction
+  } from '@solana/web3.js'
+
+export const INCEPT_EXCHANGE_SEED = Buffer.from('Incept');
+export const EXCHANGE_ADMIN = new Keypair();
+export const DEFAULT_PUBLIC_KEY = new PublicKey(0);
