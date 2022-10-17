74cd211382ac8e78b83139afd239eb4b9c5f64c6
Aron Yeager <aronyeager@protonmail.com>
2022-10-17 09:22:31 -0400

    Updating to handle reward cases.

--- programs/incept/src/instructions/recenter_comet.rs
+++ programs/incept/src/instructions/recenter_comet.rs
@@ -62,8 +62,271 @@ pub struct RecenterComet<'info> {
     pub vault: Box<Account<'info, TokenAccount>>,
     pub token_program: Program<'info, Token>,
 }
-
 pub fn execute(
+    ctx: Context<RecenterComet>,
+    manager_nonce: u8,
+    comet_position_index: u8,
+    comet_collateral_index: u8,
+) -> Result<()> {
+    let seeds = &[&[b"manager", bytemuck::bytes_of(&manager_nonce)][..]];
+    let token_data = &mut ctx.accounts.token_data.load_mut()?;
+    let mut comet = ctx.accounts.comet.load_mut()?;
+    let comet_position = comet.positions[comet_position_index as usize];
+    let comet_collateral = comet.collaterals[comet_collateral_index as usize];
+    let collateral = token_data.collaterals[comet_collateral.collateral_index as usize];
+
+    if comet.is_single_pool == 1 {
+        assert!(comet_position_index == comet_collateral_index);
+    }
+
+    // check to see if the collateral used to mint usdi is stable
+    let is_stable: Result<bool> = match collateral.stable {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => Err(error!(InceptError::InvalidBool)),
+    };
+
+    // if collateral is not stable, we throw an error
+    require!(is_stable?, InceptError::InvalidCollateralType);
+    require!(
+        comet_collateral.collateral_index as usize == USDC_COLLATERAL_INDEX,
+        InceptError::InvalidCollateralType
+    );
+
+    let iasset_amm_value = Decimal::new(
+        ctx.accounts
+            .amm_iasset_token_account
+            .amount
+            .try_into()
+            .unwrap(),
+        DEVNET_TOKEN_SCALE,
+    );
+    let usdi_amm_value = Decimal::new(
+        ctx.accounts
+            .amm_usdi_token_account
+            .amount
+            .try_into()
+            .unwrap(),
+        DEVNET_TOKEN_SCALE,
+    );
+
+    let liquidity_tokens = comet_position.liquidity_token_value.to_decimal();
+
+    let liquidity_token_supply = Decimal::new(
+        ctx.accounts.liquidity_token_mint.supply.try_into().unwrap(),
+        DEVNET_TOKEN_SCALE,
+    );
+
+    // calculate usdi and iasset comet can claim right now
+    let (claimable_iasset, claimable_usdi) =
+        calculate_liquidity_provider_values_from_liquidity_tokens(
+            liquidity_tokens,
+            iasset_amm_value,
+            usdi_amm_value,
+            liquidity_token_supply,
+        )?;
+
+    let borrowed_iasset = comet_position.borrowed_iasset.to_decimal();
+    let borrowed_usdi = comet_position.borrowed_usdi.to_decimal();
+
+    let usdi_ild = borrowed_usdi - claimable_usdi; // If negative its a reward.
+    let iasset_ild = borrowed_iasset - claimable_iasset;
+
+    require!(
+        !usdi_ild.is_zero() && !iasset_ild.is_zero(),
+        InceptError::NoPriceDeviationDetected
+    );
+
+    let mut amm_usdi_burn = borrowed_usdi;
+    let mut amm_iasset_burn = borrowed_iasset;
+
+    let mut temp_amm_usdi = usdi_amm_value - claimable_usdi;
+    let mut temp_amm_iasset = iasset_amm_value - claimable_iasset;
+    let temp_k = temp_amm_usdi * temp_amm_iasset;
+
+    let mut collateral_deficit = usdi_ild; // Start off with how much usdi ILD.
+
+    if iasset_ild.is_sign_positive() {
+        // Figure out how much usdi required to pay off ILD of iasset.
+        let usdi_to_pay = temp_k / (temp_amm_iasset - iasset_ild) - temp_amm_usdi;
+        collateral_deficit += usdi_to_pay;
+        temp_amm_usdi += usdi_to_pay;
+        temp_amm_iasset -= iasset_ild;
+    } else if iasset_ild.is_sign_negative() {
+        // Sell your extra iasset.
+        let usdi_to_receive = temp_amm_usdi - temp_k / (temp_amm_iasset - iasset_ild);
+        collateral_deficit -= usdi_to_receive;
+        temp_amm_usdi -= usdi_to_receive;
+        temp_amm_iasset -= iasset_ild;
+    }
+
+    let claimable_ratio = liquidity_tokens / liquidity_token_supply;
+
+    let mut new_borrowed_usdi =
+        temp_amm_usdi * claimable_ratio / (Decimal::one() - claimable_ratio);
+    let mut new_borrowed_iasset =
+        temp_amm_usdi * claimable_ratio / (Decimal::one() - claimable_ratio);
+
+    // Update borrowed positions.
+    new_borrowed_usdi.rescale(DEVNET_TOKEN_SCALE);
+    comet.positions[comet_position_index as usize].borrowed_usdi =
+        RawDecimal::from(new_borrowed_usdi);
+
+    new_borrowed_iasset.rescale(DEVNET_TOKEN_SCALE);
+    comet.positions[comet_position_index as usize].borrowed_iasset =
+        RawDecimal::from(new_borrowed_iasset);
+
+    // We are now minting the new borrowed amounts.
+    amm_usdi_burn -= new_borrowed_usdi;
+    amm_iasset_burn -= new_borrowed_iasset;
+
+    amm_usdi_burn.rescale(DEVNET_TOKEN_SCALE);
+    amm_iasset_burn.rescale(DEVNET_TOKEN_SCALE);
+
+    // Burn or mint for amm
+    if amm_usdi_burn.is_sign_positive() {
+        token::burn(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                Burn {
+                    mint: ctx.accounts.usdi_mint.to_account_info().clone(),
+                    from: ctx
+                        .accounts
+                        .amm_usdi_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            amm_usdi_burn.mantissa().try_into().unwrap(),
+        )?;
+    } else if amm_usdi_burn.is_sign_negative() {
+        token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                MintTo {
+                    mint: ctx.accounts.usdi_mint.to_account_info().clone(),
+                    to: ctx
+                        .accounts
+                        .amm_usdi_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            amm_usdi_burn.abs().mantissa().try_into().unwrap(),
+        )?;
+    }
+
+    if amm_iasset_burn.is_sign_positive() {
+        token::burn(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                Burn {
+                    mint: ctx.accounts.iasset_mint.to_account_info().clone(),
+                    from: ctx
+                        .accounts
+                        .amm_iasset_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            amm_iasset_burn.mantissa().try_into().unwrap(),
+        )?;
+    } else if amm_iasset_burn.is_sign_negative() {
+        token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                MintTo {
+                    mint: ctx.accounts.iasset_mint.to_account_info().clone(),
+                    to: ctx
+                        .accounts
+                        .amm_iasset_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            amm_iasset_burn.abs().mantissa().try_into().unwrap(),
+        )?;
+    }
+
+    // Update collateral.
+    collateral_deficit.rescale(DEVNET_TOKEN_SCALE);
+    let mut new_collateral_amount =
+        comet_collateral.collateral_amount.to_decimal() - collateral_deficit;
+    new_collateral_amount.rescale(DEVNET_TOKEN_SCALE);
+    comet.collaterals[comet_collateral_index as usize].collateral_amount =
+        RawDecimal::from(new_collateral_amount);
+
+    if collateral_deficit.is_sign_positive() {
+        // Transfer from vault to amm.
+        token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                Transfer {
+                    from: ctx.accounts.vault.to_account_info().clone(),
+                    to: ctx
+                        .accounts
+                        .amm_usdi_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            collateral_deficit.mantissa().try_into().unwrap(),
+        )?;
+    } else if collateral_deficit.is_sign_negative() {
+        // Transfer from amm to vault.
+        token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info().clone(),
+                Transfer {
+                    to: ctx.accounts.vault.to_account_info().clone(),
+                    from: ctx
+                        .accounts
+                        .amm_usdi_token_account
+                        .to_account_info()
+                        .clone(),
+                    authority: ctx.accounts.manager.to_account_info().clone(),
+                },
+                seeds,
+            ),
+            collateral_deficit.abs().mantissa().try_into().unwrap(),
+        )?;
+    }
+
+    // update pool data
+    ctx.accounts.amm_iasset_token_account.reload()?;
+    ctx.accounts.amm_usdi_token_account.reload()?;
+
+    token_data.pools[comet_position.pool_index as usize].iasset_amount = RawDecimal::new(
+        ctx.accounts
+            .amm_iasset_token_account
+            .amount
+            .try_into()
+            .unwrap(),
+        DEVNET_TOKEN_SCALE,
+    );
+    token_data.pools[comet_position.pool_index as usize].usdi_amount = RawDecimal::new(
+        ctx.accounts
+            .amm_usdi_token_account
+            .amount
+            .try_into()
+            .unwrap(),
+        DEVNET_TOKEN_SCALE,
+    );
+
+    Ok(())
+}
+
+pub fn _execute(
     ctx: Context<RecenterComet>,
     manager_nonce: u8,
     comet_position_index: u8,
